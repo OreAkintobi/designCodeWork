36cf7f8435fff4da56406e7651c51d48de1b7d55
OreAkintobi <oreakintobi@gmail.com>
2020-03-12 13:55:56 +0100

    added redux to implement menu transitions; created HomeScreen component

--- App.js
+++ App.js
@@ -1,215 +1,30 @@
 import React from "react";
-import { ScrollView, SafeAreaView, TouchableOpacity } from "react-native";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import styled from "styled-components";
-import Card from "./components/Card";
-import { Ionicons } from "@expo/vector-icons";
-import { NotificationIcon } from "./components/Icons";
-import Logo from "./components/Logo";
-import Course from "./components/Course";
-import Menu from "./components/Menu";
+import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
-  return (
-    <Container>
-      <Menu />
-      <SafeAreaView>
-        <ScrollView>
-          <TitleBar>
-            <Avatar source={require("./assets/avatar.jpg")} />
-            <Title>Welcome back,</Title>
-            <Name>Ore</Name>
-            <TouchableOpacity
-              style={{ position: "absolute", right: 20, top: 5 }}
-            >
-              <Ionicons name="ios-notifications" size={30} color="#546bfb" />
-            </TouchableOpacity>
-          </TitleBar>
-          <ScrollView
-            style={{
-              flexDirection: "row",
-              padding: 20,
-              paddingLeft: 12,
-              paddingTop: 40
-            }}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          >
-            {logos.map((logo, index) => (
-              <Logo key={index} image={logo.image} text={logo.text} />
-            ))}
-          </ScrollView>
-          <Subtitle>Continue Learning</Subtitle>
-          <ScrollView
-            horizontal={true}
-            style={{ paddingBottom: 30 }}
-            showsHorizontalScrollIndicator={false}
-          >
-            {cards.map((card, index) => (
-              <Card
-                key={index}
-                title={card.title}
-                image={card.image}
-                caption={card.caption}
-                logo={card.logo}
-                subtitle={card.subtitle}
-              />
-            ))}
-          </ScrollView>
-          <Subtitle>Popular Courses</Subtitle>
-          {courses.map((course, index) => (
-            <Course
-              key={index}
-              image={course.image}
-              title={course.title}
-              subtitle={course.subtitle}
-              logo={course.logo}
-              author={course.author}
-              avatar={course.avatar}
-              caption={course.caption}
-            />
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Container>
-  );
-}
+const initialState = {
+  action: ""
+};
 
-const Container = styled.View`
-  flex: 1;
-  background-color: white;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  color: #b8bece;
-  font-weight: 500;
-`;
-
-const Name = styled.Text`
-  font-size: 20px;
-  color: #3c4560;
-  font-weight: bold;
-`;
-
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 20px;
-  padding-left: 80px;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 26px;
-  position: absolute;
-  left: 0;
-  top: 0;
-`;
-
-const Subtitle = styled.Text`
-  color: #b8bece;
-  font-weight: 600;
-  font-size: 15px;
-  margin-left: 20px;
-  margin-top: 20px;
-  text-transform: uppercase;
-`;
-
-const logos = [
-  {
-    image: require("./assets/logo-framerx.png"),
-    text: "Framer X"
-  },
-  {
-    image: require("./assets/logo-figma.png"),
-    text: "Figma"
-  },
-  {
-    image: require("./assets/logo-studio.png"),
-    text: "Studio"
-  },
-  {
-    image: require("./assets/logo-react.png"),
-    text: "React"
-  },
-  {
-    image: require("./assets/logo-swift.png"),
-    text: "Swift"
-  },
-  {
-    image: require("./assets/logo-sketch.png"),
-    text: "Sketch"
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    default:
+      return state;
   }
-];
+};
 
-const cards = [
-  {
-    title: "React Native for Designers",
-    image: require("./assets/background11.jpg"),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "React for Designers",
-    image: require("./assets/background12.jpg"),
-    subtitle: "React Native",
-    caption: "2 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "Beginners Guide to Redux",
-    image: require("./assets/background13.jpg"),
-    subtitle: "React Native",
-    caption: "3 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "React Native for Beginners",
-    image: require("./assets/background14.jpg"),
-    subtitle: "React Native",
-    caption: "4 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  }
-];
+const store = createStore(reducer);
 
-const courses = [
-  {
-    title: "Prototype in InVision Studio",
-    subtitle: "10 Section",
-    image: require("./assets/background13.jpg"),
-    logo: require("./assets/logo-studio.png"),
-    author: "Ore Akintobi",
-    avatar: require("./assets/avatar.jpg"),
-    caption: "Design an interactive prototype"
-  },
-  {
-    title: "Prototype in InVision Studio",
-    subtitle: "10 Section",
-    image: require("./assets/background14.jpg"),
-    logo: require("./assets/logo-studio.png"),
-    author: "Ore Akintobi",
-    avatar: require("./assets/avatar.jpg"),
-    caption: "Design an interactive prototype"
-  },
-  {
-    title: "Prototype in InVision Studio",
-    subtitle: "10 Section",
-    image: require("./assets/background11.jpg"),
-    logo: require("./assets/logo-studio.png"),
-    author: "Ore Akintobi",
-    avatar: require("./assets/avatar.jpg"),
-    caption: "Design an interactive prototype"
-  },
-  {
-    title: "Prototype in InVision Studio",
-    subtitle: "10 Section",
-    image: require("./assets/background6.jpg"),
-    logo: require("./assets/logo-studio.png"),
-    author: "Ore Akintobi",
-    avatar: require("./assets/avatar.jpg"),
-    caption: "Design an interactive prototype"
-  }
-];
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+);
+
+export default App;
