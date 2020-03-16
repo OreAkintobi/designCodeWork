b37c2ae5c56dfa2536a6b317b07a1f31f0efece6
OreAkintobi <oreakintobi@gmail.com>
2020-03-16 11:59:11 +0100

    implemented animated project movements

--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,17 +1,147 @@
 import React from "react";
 import styled from "styled-components";
+import Project from "../components/Project";
+import { PanResponder, Animated } from "react-native";
+
+function getNextIndex(index) {
+  var nextIndex = index + 1;
+  if (nextIndex > projects.length - 1) {
+    return 0;
+  }
+  return index + 1;
+}
 
 class ProjectsScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  state = {
+    pan: new Animated.ValueXY(),
+    scale: new Animated.Value(0.9),
+    translateY: new Animated.Value(44),
+    thirdScale: new Animated.Value(0.8),
+    thirdTranslateY: new Animated.Value(-50),
+    index: 0
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+
+      onPanResponderGrant: () => {
+        Animated.spring(this.state.scale, { toValue: 1 }).start();
+        Animated.spring(this.state.translateY, { toValue: 0 }).start();
+
+        Animated.spring(this.state.thirdScale, { toValue: 0.9 }).start();
+        Animated.spring(this.state.thirdTranslateY, { toValue: 44 }).start();
+      },
+
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: this.state.pan.x, dy: this.state.pan.y }
+      ]),
+
+      onPanResponderRelease: () => {
+        const positionY = this.state.pan.y.__getValue();
+        // console.log(positionY);
+        if (positionY > 200) {
+          Animated.timing(this.state.pan, {
+            toValue: { x: 0, y: 1000 }
+          }).start(() => {
+            this.state.pan.setValue({ x: 0, y: 0 });
+            this.state.scale.setValue(0.9);
+            this.state.translateY.setValue(44);
+            this.state.thirdScale.setValue(0.8);
+            this.state.thirdTranslateY.setValue(-50);
+            this.setState({ index: getNextIndex(this.state.index) });
+          });
+        } else {
+          Animated.spring(this.state.pan, {
+            toValue: { x: 0, y: 0 }
+          }).start();
+
+          Animated.spring(this.state.scale, { toValue: 0.9 }).start();
+          Animated.spring(this.state.translateY, { toValue: 44 }).start();
+
+          Animated.spring(this.state.thirdScale, { toValue: 0.8 }).start();
+          Animated.spring(this.state.thirdTranslateY, { toValue: -50 }).start();
+        }
+      }
+    });
+  }
+
+  componentDidMount() {}
+
   render() {
     return (
       <Container>
-        <Text>Projects Screen</Text>
-        <Button
-          title="Close"
-          onPress={() => {
-            this.props.navigation.goBack();
+        <Animated.View
+          style={{
+            transform: [
+              { translateX: this.state.pan.x },
+              { translateY: this.state.pan.y }
+            ]
           }}
-        />
+          {...this._panResponder.panHandlers}
+        >
+          <Project
+            title={projects[this.state.index].title}
+            image={projects[this.state.index].image}
+            author={projects[this.state.index].author}
+            text={projects[this.state.index].text}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            transform: [
+              { scale: this.state.scale },
+              { translateY: this.state.translateY }
+            ]
+          }}
+        >
+          <Project
+            title={projects[getNextIndex(this.state.index)].title}
+            image={projects[getNextIndex(this.state.index)].image}
+            author={projects[getNextIndex(this.state.index)].author}
+            text={projects[getNextIndex(this.state.index)].text}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -3,
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            transform: [
+              { scale: this.state.thirdScale },
+              { translateY: this.state.thirdTranslateY }
+            ]
+          }}
+        >
+          <Project
+            title={projects[getNextIndex(this.state.index + 1)].title}
+            image={projects[getNextIndex(this.state.index + 1)].image}
+            author={projects[getNextIndex(this.state.index + 1)].author}
+            text={projects[getNextIndex(this.state.index + 1)].text}
+          />
+        </Animated.View>
       </Container>
     );
   }
@@ -23,8 +153,38 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background: #f0f3f5;
 `;
 
 const Text = styled.Text``;
 
 const Button = styled.Button``;
+
+const projects = [
+  {
+    title: "Price Tag",
+    image: require("../assets/background5.jpg"),
+    author: "By Olaolu Akintobi",
+    text:
+      "Thank you Mr. Laolu Akins, you are a godsend, you are blessed. Thank you sir"
+  },
+  {
+    title: "ContactMe",
+    image: require("../assets/background6.jpg"),
+    author: "By Olamide Akintobi",
+    text: "Thank you Sis Mims, you are a godsend, you are blessed. Thank you ma"
+  },
+  {
+    title: "Better Living",
+    image: require("../assets/background7.jpg"),
+    author: "By Olumayowa Akintobi",
+    text: "Thank you Mrs Yoyo, you are a godsend, you are blessed. Thank you ma"
+  },
+  {
+    title: "SuperTravel",
+    image: require("../assets/background8.jpg"),
+    author: "By Olubukunola Akintobi",
+    text:
+      "Thank you Madam Bukky, you are a godsend, you are blessed. Thank you madam"
+  }
+];
